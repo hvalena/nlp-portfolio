b3f50a14cbf3220afdd92584de9c083285830250
hvalena <hannah.valena@utdallas.edu>
2022-09-29 18:06:35 -0500

    added homework5 files, instructions, and program 1

---
+++ Homework5/ngrams/program_2.py
@@ -0,0 +1,11 @@
+# CS 4395.001 Portfolio Chapter 8: Ngrams
+# Written by: Ryan Dimaranan and Hannah Valena
+
+def main():
+    # read in pickled dictionaries
+    print('hi')
+
+
+if __name__ == '__main__':
+    main()
+
